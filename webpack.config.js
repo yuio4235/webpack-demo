aab06b34cc1f036d4bd3d13ece09473e2d4d445d
yuio4235 <yuio4235@gmail.coom>
2020-03-12 02:23:13 +0800

    load fonts file with file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,12 @@ module.exports = {
                 use: [
                     'file-loader'
                 ]
+            },
+            {
+                test: /\.(woff|woff2)$/,
+                use: [
+                    'file-loader'
+                ]
             }
         ]
     }
