fc4eb57706538451400437a96f56f1c30881457a
yuio4235 <yuio4235@gmail.coom>
2020-03-12 17:18:17 +0800

    enable webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,9 @@ module.exports = {
         print: './src/print.js'
     },
     devtool: 'inline-source-map',
+    devServer: {
+        contentBase: '/'
+    },
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
