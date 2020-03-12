ba6b4b72a7cd8ba6a7f4cc376e49eeaa40dc3456
yuio4235 <yuio4235@gmail.coom>
2020-03-12 15:43:59 +0800

    useage of html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: {
@@ -8,5 +9,8 @@ module.exports = {
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({title: 'Output Management'})
+    ]
 };
