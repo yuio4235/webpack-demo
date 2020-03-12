dcc532729e3448c46ebc522f626dff5213d326b5
yuio4235 <yuio4235@gmail.coom>
2020-03-12 16:35:30 +0800

    useage of webpack-manifest-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin')
 
 module.exports = {
     entry: {
@@ -13,6 +14,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({title: 'Output Management'}),
-        new CleanWebpackPlugin()
+        new CleanWebpackPlugin(),
+        new ManifestPlugin()
     ]
 };
