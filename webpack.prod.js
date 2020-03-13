d7a21f89a5405065b2827d8680a99492c0e96a2c
yuio4235 <yuio4235@gmail.coom>
2020-03-13 16:13:47 +0800

    bundle体积压缩

---
+++ webpack.prod.js
@@ -0,0 +1,14 @@
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+const UglifyPlugin = require('uglifyjs-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = merge(common, {
+    mode: "production",
+    plugins: [
+    ],
+    optimization: { //这个地方不用再使用UglifyPlugin,这个会增加bundle的体积，默认现在用的TerserPlugin
+        runtimeChunk: true, 
+        splitChunks: {chunks: "all", maxSize: 1200000}
+    }
+});
