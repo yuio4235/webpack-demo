c6fb706d3a0a6d67a1f16c50e3ca6295b0c70a22
yuio4235 <yuio4235@gmail.coom>
2020-03-13 11:26:06 +0800

    tree shaking

--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
 import _ from "lodash";
 import printMe from './print.js';
 import "./style.css";
+import {cube} from "./math.js";
 
 function component() {
-    var element = document.createElement('div');
+    var element = document.element.createElement('pre');
 
-    var btn = document.createElement('button');
-
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-    btn.innerHTML = 'Click me and check the consoel!';
-
-    btn.onclick = printMe;
-
-    element.appendChild(btn);
+    element.innerHTML = [
+        'Hello webpack!',
+        '5 cubed is equal to ' + cube(5)
+    ].join('\n\n');
 
     return element;
 }
